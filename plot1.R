badf7c626194597f79240c18bf95b55f3246ded6
Vijaya Kumar Bhavani <vbhavani@users.noreply.github.com>
2015-10-12 01:10:19 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# download data
+library(dplyr)
+sFile <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+if (!file.exists("data/household_power_consumption.zip")) {
+  download.file(sFile, destfile = "data/household_power_consumption.zip")
+}
+
+dt <-read.table("data/household_power_consumption.txt",sep = ";", header=TRUE, na.strings = c("?"),
+                stringsAsFactors = FALSE, dec = ".")
+
+dt$DateTime <- strptime(paste(dt$Date,dt$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+dt2 <- dt[dt$DateTime >= "2007-02-01 00:00:00" & dt$DateTime <= "2007-02-02 23:59:59",]
+png("plot1.png", width=480, height=480)
+with(dt2,hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
+dev.off()
